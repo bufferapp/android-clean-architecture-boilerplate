3b2e9b151c92708cea652c17faabd1aa48281b46
Martin Bonnin <martin@mbonnin.net>
2018-10-13 21:39:53 +0200

    fix comments

--- data/src/main/java/org/buffer/android/boilerplate/data/repository/BufferooRemote.kt
+++ data/src/main/java/org/buffer/android/boilerplate/data/repository/BufferooRemote.kt
@@ -4,13 +4,13 @@ import io.reactivex.Single
 import org.buffer.android.boilerplate.data.model.BufferooEntity
 
 /**
- * Interface defining methods for the caching of Bufferroos. This is to be implemented by the
- * cache layer, using this interface as a way of communicating.
+ * Interface defining methods for getting Bufferoos remotely. This is to be implemented by the
+ * remote layer, using this interface as a way of communicating.
  */
 interface BufferooRemote {
 
     /**
-     * Retrieve a list of Bufferoos, from the cache
+     * Retrieve a list of Bufferoos, from the network or any other remote
      */
     fun getBufferoos(): Single<List<BufferooEntity>>
 
